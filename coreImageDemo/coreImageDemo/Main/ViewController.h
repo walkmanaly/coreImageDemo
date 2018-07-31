d43db9b83dccb117e6d83afd33afa1c72e96e6df
Nick <walkmanaly@gmail.com>
2018-07-31 14:31:52 +0800

    init project
    init project

---
+++ coreImageDemo/coreImageDemo/Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreImageDemo
+//
+//  Created by Nick on 2018/7/31.
+//  Copyright © 2018年 nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coreImageDemo/coreImageDemo/SoportingFiles/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coreImageDemo
+//
+//  Created by Nick on 2018/7/31.
+//  Copyright © 2018年 nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
